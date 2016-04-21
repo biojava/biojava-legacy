39c05450dde56d3d5b48a92b01dc50b7f2960f50
gwaldon <gwaldon@geneinfinity.org>
2016-04-21 11:24:07 -0700

    Editing of ChunkedSymbolList plus tests.

--- core/src/main/java/org/biojava/bio/symbol/ChunkedSymbolList.java
+++ core/src/main/java/org/biojava/bio/symbol/ChunkedSymbolList.java
@@ -2,8 +2,12 @@ package org.biojava.bio.symbol;
 
 import java.io.IOException;
 import java.io.Serializable;
+import org.biojava.bio.seq.io.ChunkedSymbolListFactory;
+import org.biojava.utils.ChangeEvent;
 
 import org.biojava.utils.ChangeListener;
+import org.biojava.utils.ChangeSupport;
+import org.biojava.utils.ChangeVetoException;
 
 /**
  * SymbolList implementation using constant-size chunks. Each chunk provides
@@ -18,6 +22,7 @@ import org.biojava.utils.ChangeListener;
  *
  * @author David Huen
  * @author Matthew Pocock
+ * @author George Waldon
  */
 public class ChunkedSymbolList
         extends AbstractSymbolList
@@ -27,7 +32,7 @@ public class ChunkedSymbolList
   private SymbolList [] chunks;
   private final int chunkSize;
   private final Alphabet alpha;
-  private final int length;
+  private int length;
 
   // cached info for speedups
   private transient int currentMin = Integer.MAX_VALUE;
@@ -116,4 +121,118 @@ public class ChunkedSymbolList
       return super.subList(start, end);
     }
   }
+  
+    public void edit(Edit edit) throws IllegalAlphabetException,
+            ChangeVetoException {
+        ChangeSupport cs;
+        ChangeEvent cevt;
+        Symbol[] dest;
+
+        // first make sure that it is in bounds
+        if ((edit.pos + edit.length > length +1 ) || (edit.pos <= 0) || edit.length < 0){
+            throw new IndexOutOfBoundsException();
+        }
+        // make sure that the symbolList is of the correct alphabet
+        if (( edit.replacement.getAlphabet() != getAlphabet()) &&  (edit.replacement != SymbolList.EMPTY_LIST)){
+            throw new IllegalAlphabetException();
+        }
+
+        // give the listeners a change to veto this
+         // create a new change event ->the EDIT is a static final variable of type ChangeType in SymbolList interface
+        cevt = new ChangeEvent(this, SymbolList.EDIT, edit);
+        cs = getChangeSupport(SymbolList.EDIT);
+        synchronized(cs) {
+            // let the listeners know what we want to do
+            cs.firePreChangeEvent(cevt);
+        
+            // now for the edit
+            int posRightFragInSourceArray5 = edit.pos + edit.length - 1;
+            int rightFragLength = length - posRightFragInSourceArray5;
+            int posRightFragInDestArray5 = posRightFragInSourceArray5 + edit.replacement.length() - edit.length;
+            int posReplaceFragInDestArray5 = edit.pos - 1;
+            int replaceFragLength = edit.replacement.length();
+            int newLength = length + replaceFragLength - edit.length;
+
+            // extend the array
+            dest = new Symbol[newLength];
+            
+            // copy symbols before the edit and make sure we are not editing the edit at the same time (hoops!)
+            int leftFragLength = edit.pos - 1;
+            for (int i = 0; i < chunks.length; i++) {
+                SymbolList chunk = chunks[i];
+                Symbol[] chunkSymbol = ((SimpleSymbolList) chunk).getSymbolArray();
+                int desStart = i * chunkSize;
+                int desEnd = i * chunkSize + chunk.length();
+                if (desEnd >= leftFragLength) {
+                    desEnd = leftFragLength;
+                    System.arraycopy(chunkSymbol, 0, dest, desStart, desEnd - desStart);
+                    break;
+                }
+                System.arraycopy(chunkSymbol, 0, dest, desStart, desEnd - desStart);
+            }
+            
+            // copy the symbols after the edit
+            if (rightFragLength > 0) {
+                int chunkNum = posRightFragInSourceArray5 / chunkSize;
+                int chunkStart = posRightFragInSourceArray5 % chunkSize;
+                int chunkCopied = 0;
+                for (int i = chunkNum; i < chunks.length; i++) {
+                    SymbolList chunk = chunks[i];
+                    Symbol[] chunkSymbol = ((SimpleSymbolList) chunk).getSymbolArray();
+                    int desStart = posRightFragInDestArray5 + chunkCopied;
+                    int desLength = chunk.length() - chunkStart;
+                    int left = rightFragLength - chunkCopied;
+                    if (left == 0) {
+                        break;
+                    }
+                    if (desLength > left) {
+                        desLength = left;
+                    }
+                    System.arraycopy(chunkSymbol, chunkStart, dest, desStart, desLength);
+                    if (chunkStart != 0) {
+                        chunkStart = 0;
+                    }
+                    chunkCopied += desLength;
+                }
+            }
+
+            // copy the symbols within the edit
+            for (int i = 1; i <= replaceFragLength; i++) {
+                dest[posReplaceFragInDestArray5 + i - 1] = edit.replacement.symbolAt(i);
+            }
+
+            //Make the chunks
+            int chunknum = newLength/chunkSize;
+            int chunkleft = newLength%chunkSize;
+            int chunkReused = (edit.pos-1)/chunkSize;
+            if(chunkleft>0) chunknum++;
+            SymbolList [] symListArray = new SymbolList[chunknum];
+            for(int i=0;i<chunknum-1;i++) {
+                if(i<chunkReused) {
+                    symListArray[i] = chunks[i];
+                    continue;
+                }
+                Symbol[] chunk = new Symbol[chunkSize];
+                System.arraycopy(dest, i*chunkSize, chunk, 0, chunkSize);
+                symListArray[i] = new SimpleSymbolList(chunk, chunkSize, getAlphabet());
+            }
+            if(chunkleft>0) {
+                Symbol[] chunk = new Symbol[chunkleft];
+                System.arraycopy(dest, (chunknum-1)*chunkSize, chunk, 0, chunkleft);
+                symListArray[chunknum-1] = new SimpleSymbolList(chunk, chunkleft, getAlphabet());
+            } else {
+                Symbol[] chunk = new Symbol[chunkSize];
+                System.arraycopy(dest, (chunknum-1)*chunkSize, chunk, 0, chunkSize);
+                symListArray[chunknum-1] = new SimpleSymbolList(chunk, chunkSize, getAlphabet());
+            }
+
+            chunks = symListArray;
+            length = newLength;
+            currentMin = Integer.MAX_VALUE;
+            currentMax = Integer.MIN_VALUE;
+            currentChunk = null;
+
+            cs.firePostChangeEvent(cevt);
+        }
+    }
 }
